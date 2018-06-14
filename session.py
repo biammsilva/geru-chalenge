cd9c54f1463d4d4f50e6386fdad7e4aaf882d4b0
Bia <beatriz.mm.silva@gmail.com>
2018-06-13 22:48:44 -0300

    deleting old files

--- session.py
+++
@@ -1,6 +0,0 @@
-from pyramid.session import SignedCookieSessionFactory
-from pyramid.config import Configurator
-
-def initialize_session(config):
-    my_session_factory = SignedCookieSessionFactory('itsaseekreet')
-    config.set_session_factory(my_session_factory)
